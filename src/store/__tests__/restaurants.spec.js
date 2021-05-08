234d376e06a58d45dcec8ca771f12031ae841ecd
Andre Eli <andre.eli@khk.ee>
2021-05-08 23:54:06 +0300

    Added server errors and styles

--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -162,4 +162,12 @@ describe('createRestaurant action', () => {
       return expect(promise).resolves.toBeUndefined();
     });
   });
+
+  describe('when save fails', () => {
+    it('rejects', () => {
+      api.createRestaurant.mockRejectedValue();
+      promise = store.dispatch(createRestaurant(newRestaurantName));
+      return expect(promise).rejects.toBeUndefined();
+    });
+  });
 });
