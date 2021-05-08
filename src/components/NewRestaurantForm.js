4a19dd32d9e0bbd6758537157d429a0152842f97
Andre Eli <andre.eli@khk.ee>
2021-05-08 23:29:48 +0300

    Added validation

--- src/components/NewRestaurantForm.js
+++ src/components/NewRestaurantForm.js
@@ -1,17 +1,30 @@
 import {useState} from 'react';
+import {connect} from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
+import {createRestaurant} from '../store/restaurants/actions';
 
 export const NewRestaurantForm = ({createRestaurant}) => {
   const [name, setName] = useState('');
+  const [validationError, setValidationError] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
-    createRestaurant(name);
+
+    if (name) {
+      setValidationError(false);
+      createRestaurant(name).then(() => {
+        setName('');
+      });
+    } else {
+      setValidationError(true);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      {validationError && <Alert severity="error">Name is required</Alert>}
       <TextField
         value={name}
         onChange={e => setName(e.target.value)}
@@ -31,4 +44,7 @@ export const NewRestaurantForm = ({createRestaurant}) => {
   );
 };
 
-export default NewRestaurantForm;
+const mapStateToProps = null;
+const mapDispatchToProps = {createRestaurant};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewRestaurantForm);
